737ab0dd0170a97534e0daa43c7a5124632d6e7c
CHAITRA N <165247679+Chaitra0407@users.noreply.github.com>
2024-07-27 18:55:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const fileupload = require('express-fileupload');
+let initial_path = path.join(__dirname,"public");
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(initial_path,"home.html"));
+})
+app.get('/editor',(req,res)=>{
+    res.sendFile(path.join(initial_path,"editor.html"));
+})
+app.post('/upload',(req,res)=> {
+
+    let file = req.files.image;
+    let date = new Date();
+    let imagename=date.getDate()+date.getTime()+file.name;
+    let path ='public/uploads/'+imagename;
+    file.mv(path,(err,result) => {
+        if(err){
+            throw err;
+       }
+       else
+       {
+        res.json(`uploads/${imagename}`)
+       }
+    })
+})
+ 
+app.get("/:blog",(req,res) => {
+    res.sendFile(path.join(initial_path,"blog.html"));
+})
+app.use((req,res) => {
+    res.json("404");
+})
+app.listen("2000",()=> {
+    console.log('listening');
+});
